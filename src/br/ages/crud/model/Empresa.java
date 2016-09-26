0f2641c1909de2458928527eeb296784313490a8
13104443 <13104443@W3070554.portoalegre.pucrsnet.br>
2016-09-26 19:56:26 -0300

    model da empresa

---
+++ src/br/ages/crud/model/Empresa.java
@@ -0,0 +1,97 @@
+package br.ages.crud.model;
+
+import java.util.Date;
+
+public class Empresa {
+	private int idEmpresa;
+	private int cnpj;
+	private String telefone;
+	private String nome;
+	private String endereco;
+	private String cidade;
+	private String razaoSocial;
+	
+	private Date dataAlteracao;
+	
+	public Empresa(){
+		this.dataAlteracao = new Date();
+	}
+
+
+
+	public Empresa(int idEmpresa, int cnpj, String nome, String telefone, String endereco, String cidade, String razaoSocial) {
+		this.idEmpresa = idEmpresa;
+		this.cnpj = cnpj;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.cidade = cidade;
+		this.razaoSocial = razaoSocial;
+		this.nome = nome;
+		this.dataAlteracao = new Date();
+	}
+
+	public int getIdEmpresa() {
+		return idEmpresa;
+	}
+
+	public void setIdEmpresa(int idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+
+	public int getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Empresa" + " CNPJ=" + cnpj + ", Nome=" + nome + ", Cidade="
+				+ cidade + ", Razao Social=" + razaoSocial + ", Telefone=" + telefone
+				+ ", Endereco=" + endereco + "]";
+	}
+	
+}
