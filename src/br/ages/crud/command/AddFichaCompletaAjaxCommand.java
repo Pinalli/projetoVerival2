c2b0e339884dc48d5dc2c860cbf8d96f2b42c269
alessandro_medeiros <alessandro_medeiros@acad.pucrs.br>
2016-11-21 22:27:15 -0200

    int cadastrar ficha completa BO

--- src/br/ages/crud/command/AddFichaCompletaAjaxCommand.java
+++ src/br/ages/crud/command/AddFichaCompletaAjaxCommand.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+
 import com.google.gson.Gson;
+
 import br.ages.crud.bo.FichaCompletaBO;
+import br.ages.crud.bo.FichaCompletaItemBO;
 import br.ages.crud.model.Ficha;
 import br.ages.crud.model.FichaItem;
 import br.ages.crud.util.MensagemContantes;
@@ -15,21 +20,23 @@ import br.ages.crud.util.MensagemContantes;
 public class AddFichaCompletaAjaxCommand implements Command {
 
 	Logger logger = Logger.getLogger("AddFichaCompletaAjaxCommand");
-	Gson gson = new Gson();
+	Gson gson = new Gson();	
 	private String proxima;
 
 	private FichaCompletaBO fichaCompletaBO;
+	private FichaCompletaItemBO itemBO;
 
 	@Override
 	public String execute(HttpServletRequest request) {
 		fichaCompletaBO = new FichaCompletaBO();
+		itemBO = new FichaCompletaItemBO();
 		String json = "";
 		Map<String,String> msg = new HashMap<String, String>();
 
 		try {
 			String jsonItens = request.getParameter("itens");
 
-			
+						
 			FichaItem[] itens = gson.fromJson(jsonItens, FichaItem[].class);
 			
 			List<FichaItem> listaFichaItens = new ArrayList<FichaItem>();
@@ -52,22 +59,26 @@ public class AddFichaCompletaAjaxCommand implements Command {
 			ficha.setItens(listaFichaItens);
 			
 			if(fichaCompletaBO.validarFichaCompleta(ficha)){
-				fichaCompletaBO.cadastrarFichaCompleta(ficha);
+				int id = fichaCompletaBO.cadastrarFichaCompleta(ficha);
+				for(FichaItem item: listaFichaItens){
+					item.setIdFicha(id);
+					itemBO.cadastrarFichaCompleta(item);
+				}
 				msg.put("mensagem", MensagemContantes.MSG_SUC_CADASTRO_FICHA_COMPLETA.replace("?", ficha.getNome()));
-				msg.put("dados", "");
-				msg.put("proxima", "main?acao=listFichaCompleta");
-				json = gson.toJson(msg);
+			    msg.put("dados", "");
+			    msg.put("proxima", "main?acao=listFichaCompleta");
+			    json = gson.toJson(msg);
 			}else{
 				msg.put("mensagem", MensagemContantes.MSG_ERR_FICHA_COMPLETA_DADOS_INVALIDOS);
-				msg.put("erro", MensagemContantes.MSG_ERR_FICHA_COMPLETA_DADOS_INVALIDOS);
-				msg.put("proxima","main?acao=telaFichaCompleta");
-				json = gson.toJson(msg);
+			    msg.put("erro", MensagemContantes.MSG_ERR_FICHA_COMPLETA_DADOS_INVALIDOS);
+			    msg.put("proxima", "main?acao=telaFichaCompleta");
+			    json = gson.toJson(msg);
 			}
 			return json;
 		} catch (Exception e) {
-			msg.put("mensagem", MensagemContantes.MSG_ERR_FICHA_COMPLETA_DADOS_INVALIDOS);
-			msg.put("erro",e.getMessage());
-			msg.put("proxima", "main?acao=telaFichaCompleta");
+		    msg.put("mensagem", MensagemContantes.MSG_ERR_FICHA_COMPLETA_DADOS_INVALIDOS);
+		    msg.put("erro", e.getMessage());
+		    msg.put("proxima", "main?acao=telaFichaCompleta");
 			json = gson.toJson(msg);
 			return json;
 		}
